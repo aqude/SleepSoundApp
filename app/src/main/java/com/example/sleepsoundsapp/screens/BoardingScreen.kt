4134c707b41be13a7003e13f29df706e87b37d0d
aqude <aqu.de@yandex.ru>
2023-03-31 02:28:54 +0300

    add Indicator

--- app/src/main/java/com/example/sleepsoundsapp/screens/BoardingScreen.kt
+++ app/src/main/java/com/example/sleepsoundsapp/screens/BoardingScreen.kt
@@ -1,15 +1,19 @@
 package com.example.sleepsoundsapp.screens
 
 import android.graphics.pdf.PdfDocument.Page
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.Indication
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -50,9 +54,19 @@ fun BoardingScreen(navController: NavHostController) {
             "Знаменитые сказки с успокаивающими звуками помогут вашим детям быстрее заснуть"
         )
     )
-    
-    val pagerState = rememberPagerState(pageCount = items.size, initialOffscreenLimit = 2, infiniteLoop = false, initialPage = 0)
-    BoardingScreenPager(item = items, pagerState, modifier = Modifier.fillMaxSize().background(color = mainWallpaperColor).padding(horizontal = 24.dp))
+
+    val pagerState = rememberPagerState(
+        pageCount = items.size,
+        initialOffscreenLimit = 2,
+        infiniteLoop = false,
+        initialPage = 0
+    )
+    BoardingScreenPager(
+        item = items, pagerState, modifier = Modifier
+            .fillMaxSize()
+            .background(color = mainWallpaperColor)
+            .padding(horizontal = 24.dp)
+    )
 }
 
 
@@ -64,20 +78,51 @@ fun BoardingScreenPager(
     modifier: Modifier = Modifier,
     modifierText: Modifier = Modifier,
 ) {
-    Box(modifier = modifier, ){
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+    Box(modifier = modifier) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally)
+        {
             HorizontalPager(state = pagerState) { page ->
-                Column(modifier = Modifier
-                    .padding(top = 60.dp)
-                    .fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painter = painterResource(id = item[page].Image), contentDescription = item[page].Title)
+                Column(
+                    modifier = Modifier
+                        .padding(top = 60.dp)
+                        .fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        painter = painterResource(id = item[page].Image),
+                        contentDescription = item[page].Title
+                    )
                     Text(text = item[page].Title, color = Color.White, fontSize = 34.sp)
                     Text(text = item[page].Text, color = Color.White, fontSize = 16.sp)
                 }
-                
+
             }
+            PagerIndicator(item.size, pagerState.currentPage)
         }
     }
 }
 
+@Composable
+fun PagerIndicator(size: Int, currentPage: Int) {
+    Row(modifier = Modifier.padding(10.dp), horizontalArrangement = Arrangement.SpaceBetween) {
+        repeat(size) {
+            Indicator(isSelected = it == currentPage)
+        }
+    }
+}
+
+@Composable
+
+fun Indicator(isSelected: Boolean) {
+    val width = animateDpAsState(targetValue = if (isSelected) 20.dp else 10.dp)
+    Box(
+        modifier = Modifier
+            .padding(1.dp)
+            .height(10.dp)
+            .width(width.value)
+            .clip(CircleShape)
+            .background(
+                if (isSelected) Color(0xFF4870FF) else Color(0xFF21283F)
+            )
+    )
+}
 

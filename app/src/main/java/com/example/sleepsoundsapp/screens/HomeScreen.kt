ab075dfcdda4d2da8cd724ad225985ae7bdcd00e
aqude <aqu.de@yandex.ru>
2023-04-02 22:49:59 +0300

    create MainContent

--- app/src/main/java/com/example/sleepsoundsapp/screens/HomeScreen.kt
+++ app/src/main/java/com/example/sleepsoundsapp/screens/HomeScreen.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
@@ -21,6 +23,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.sleepsoundsapp.R
+import com.example.sleepsoundsapp.utils.ContentHomeData
 import com.example.sleepsoundsapp.utils.TabBarTopData
 
 @Composable
@@ -29,6 +32,32 @@ import com.example.sleepsoundsapp.utils.TabBarTopData
 fun HomeScreen() {
     val backgroundColor: Color = Color(0xFF141927)
     val topBarBlocks = ArrayList<TabBarTopData>()
+    val contentHomeData = ArrayList<ContentHomeData>()
+
+    contentHomeData.add(
+        ContentHomeData(
+            Color.Cyan,
+            "Guitar Camp",
+            3,
+            "Instrumental"
+        )
+    )
+    contentHomeData.add(
+        ContentHomeData(
+            Color.White,
+            "Guitar Camp",
+            1,
+            "Ambient"
+        )
+    )
+    contentHomeData.add(
+        ContentHomeData(
+            Color.Red,
+            "Guitar Camp",
+            5,
+            "Instrumental"
+        )
+    )
 
     topBarBlocks.add(
         TabBarTopData(
@@ -56,6 +85,7 @@ fun HomeScreen() {
     ) {
         Column(modifier = Modifier.padding(horizontal = 16.dp)) {
             TabBar(topBarBlocks)
+            MainContent(contentHomeData)
         }
     }
 }
@@ -113,8 +143,28 @@ fun TabBar(blocks: ArrayList<TabBarTopData>) {
 }
 
 @Composable
-fun MainContent() {
+fun MainContent(items: ArrayList<ContentHomeData>) {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            contentPadding = PaddingValues(start = 7.5.dp, end = 7.5.dp, bottom = 100.dp),
+            modifier = Modifier.fillMaxHeight()
 
+        ) {
+            items(items.size) {
+                ContentItem(items = items[it])
+            }
+        }
+    }
+
+}
+
+@Composable
+fun ContentItem(items: ContentHomeData) {
+    TODO(
+        "https://github.com/philipplackner/MeditationUIYouTube/blob/master/app/src/main/java/com/plcoding/meditationuiyoutube/ui/HomeScreen.kt"
+    + "додедать как в коде FeatureItem"
+    )
 }
 
 fun BottomNavBar() {
---
+++ app/src/main/java/com/example/sleepsoundsapp/utils/ContentHomeData.kt
@@ -0,0 +1,11 @@
+package com.example.sleepsoundsapp.utils
+
+import androidx.compose.ui.graphics.Color
+
+data class ContentHomeData(
+//    val backgroundImg: Int,
+    val backgroundColor: Color,
+    val title: String,
+    val timeInfo: Int,
+    val categoryName: String
+)
